6160973f70718b901f602027966b6a42a3a35392
cgale2 <cgale2@kent.edu>
2026-03-27 23:19:51 -0400

    email tempate for waivers

--- scripts/emails.py
+++ scripts/emails.py
@@ -2,15 +2,11 @@ import smtplib
 import pymongo
 from email.message import EmailMessage
 
+# Email parameters
 msg = EmailMessage()
-msg.set_content("Hello, this is a test email from Python!")
 msg["Subject"] = "Python Test"
 msg["From"] = "charlesdgale@gmail.com"
 msg["To"] = "cgale2@kent.edu"
-
-with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-    smtp.login("charlesdgale@gmail.com", "ggjl hnef nutj nswn")
-    smtp.send_message(msg)
 
 # Mongo client object with connection URL
 myclient = pymongo.MongoClient("mongodb://localhost:27017")
@@ -18,9 +14,16 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017")
 # The actual database
 mydb = myclient["rollcall"]
 
-print(mydb.list_collection_names())
+# Query for filtering
+myquery = {"decision": "approved"}
 
-# Use this to send personalized emails when there is data in it
+# Print all items in collection
 mycol = mydb["waiver_approvals"]
-for document in mycol.find():
-    print(document)
+for x in mycol.find(myquery):
+    # Email body
+    msg.set_content(str(x))
+
+    # Send email
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login("charlesdgale@gmail.com", "") # Put your own code in the empty ""
+        smtp.send_message(msg)
