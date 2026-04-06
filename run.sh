d3eac52f360ddd64d370264f4457d74cf2e12902
TJ Raklovits <tjraklovits@gmail.com>
2026-04-06 19:57:21 -0400

    modify workflow to call run script

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 pkill streamlit
 cd ~/rollcall
 git pull
+pip install -r requirements.txt
 nohup streamlit run Home.py --server.port 15084 --server.address 0.0.0.0 2>&1 out &
 mongod --dbpath ~/mongodb/data --logpath ~/mongodb/logs/mongod.log --port 27017 --fork
