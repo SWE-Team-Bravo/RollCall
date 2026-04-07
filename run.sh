1e7bbd0e7b99798e7b72f61b313544cc8696ad84
Monster0506 <tjraklovits@gmail.com>
2026-04-06 20:53:27 -0400

    fix deployment script

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ pkill streamlit
 cd ~/rollcall
 git pull
 pip install -r requirements.txt
-nohup streamlit run Home.py --server.port 15084 --server.address 0.0.0.0 2>&1 out &
+nohup streamlit run Home.py --server.port 15084 --server.address 0.0.0.0 > out 2>&1 &
 mongod --dbpath ~/mongodb/data --logpath ~/mongodb/logs/mongod.log --port 27017 --fork
