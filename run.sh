9f0d0d854b67ac75f5a18928dc75b43ba412b30e
Monster0506 <tjraklovits@gmail.com>
2026-04-06 21:01:58 -0400

    change to absolute paths for streamlit and mongodb

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -l
+source ~/.bashrc
 
 pkill streamlit
 cd ~/rollcall
 git pull
 pip install -r requirements.txt
-nohup streamlit run Home.py --server.port 15084 --server.address 0.0.0.0 > out 2>&1 &
+nohup /home/rollcall/.local/bin/streamlit run Home.py --server.port 15084 --server.address 0.0.0.0 > out 2>&1 &
+/home/rollcall/.local/bin/mongod --dbpath ~/mongodb/data --logpath ~/mongodb/logs/mongod.log --port 27017 --fork
