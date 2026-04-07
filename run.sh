86532d79d565bb3668895a23418a4ccb578ba848
Monster0506 <tjraklovits@gmail.com>
2026-04-06 20:58:14 -0400

    fix deploy script to run in a login shell

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/bash -l
 
 pkill streamlit
 cd ~/rollcall
 git pull
 pip install -r requirements.txt
 nohup streamlit run Home.py --server.port 15084 --server.address 0.0.0.0 > out 2>&1 &
-mongod --dbpath ~/mongodb/data --logpath ~/mongodb/logs/mongod.log --port 27017 --fork
