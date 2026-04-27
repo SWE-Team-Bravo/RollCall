55964ad0554e9f2d7910df491e7a59487db5fa23
Monster0506 <tjraklovits@gmail.com>
2026-04-27 08:48:27 -0400

    Revert "hotfix: fix failing e2e tests?"
    This reverts commit 31eb7b9f2a8872b4f5f45fffc5781e7e675bd12e.

--- tests/test_event_generation.py
+++ tests/test_event_generation.py
@@ -24,7 +24,7 @@ _STREAMLIT = str(
 )
 
 URL = "http://localhost:15085"
-EVENT_MGMT_URL = f"{URL}/Event_Management_CRUD"
+EVENT_MGMT_URL = f"{URL}/Event_Management"
 
 pytestmark = pytest.mark.e2e
 
