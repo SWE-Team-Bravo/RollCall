619a8f8b5a980f66931290629582723f24131d90
Brent Stanfield <brentstanfield9@outlook.com>
2026-04-28 12:52:08 -0400

    refactor html theme overrides closes #293

--- config/settings.py
+++ config/settings.py
@@ -7,3 +7,13 @@ APP_NAME = "RollCall"
 MONGODB_URI = os.getenv("MONGODB_URI")
 MONGODB_DB = os.getenv("MONGODB_DB", "rollcall_db")
 AUTH_COOKIE_KEY = os.getenv("AUTH_COOKIE_KEY")
+
+HTML_THEME_OVERRIDE_COLORS = {
+    "primary_button_enabled_text": "#000000",
+    "primary_button_disabled_text_dark": "#f2f2f2",
+    "primary_button_disabled_text_light": "#000000",
+    "multiselect_tag_text": "#000000",
+    "expander_border": "rgba(0, 0, 0, 0.12)",
+    "expander_summary_background": "rgba(0, 0, 0, 0.08)",
+    "expander_summary_hover_background": "rgba(0, 0, 0, 0.12)",
+}
